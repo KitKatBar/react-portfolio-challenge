d9e78d2708eceaa562c50efaa267e70b3cabf8cf
Katriel Chiu <katriel_chiu@msn.com>
2024-05-29 20:32:46 -0700

    getting rid of annoying warning

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,22 +7,22 @@ function Footer() {
             <address class="contact">
                 <ul>
                     <li>
-                        <a id="phone" href="tel:my-number-here"></a>
+                        <a id="phone" href="tel:my-number-here"> </a>
                     </li>
                     <li>
-                        <a id="email" href="mailto:katriel_chiu@msn.com"></a>
+                        <a id="email" href="mailto:katriel_chiu@msn.com"> </a>
                     </li>
                     <li>
-                        <a id="github" href="https://github.com/KitKatBar/"></a>
+                        <a id="github" href="https://github.com/KitKatBar/"> </a>
                     </li>
                     <li>
-                        <a id="linkedin" href="https://www.linkedin.com/in/katriel-chiu-a8592aa8/"></a>
+                        <a id="linkedin" href="https://www.linkedin.com/in/katriel-chiu-a8592aa8/"> </a>
                     </li>
                     <li>
-                        <a id="discord" href="https://discord.com/users/117181812450525190"></a>
+                        <a id="discord" href="https://discord.com/users/117181812450525190"> </a>
                     </li>
                     <li>
-                        <a id="youtube" href="https://www.youtube.com/channel/UCgPOWio1gCV7e5ohrsP-Qgw"></a>
+                        <a id="youtube" href="https://www.youtube.com/channel/UCgPOWio1gCV7e5ohrsP-Qgw"> </a>
                     </li>
                 </ul>
             </address>
