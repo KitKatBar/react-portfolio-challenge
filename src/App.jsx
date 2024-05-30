bc659386df10a74a0a51cfbbb6018c443aee693f
Katriel Chiu <katriel_chiu@msn.com>
2024-05-29 20:22:26 -0700

    added a footer to the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Header  from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+      <main className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </main>
+      <Footer />
     </div>
   );
 }
