cf15769a9dd4ff856afc98ad6670b8a49058ec77
Katriel Chiu <katriel_chiu@msn.com>
2024-05-30 04:05:09 -0700

    added in a logo and updated navigation bar to highlight the current page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,6 +7,7 @@ function Header(props) {
             <h1>
                 KitKatBar's Site
             </h1>
+            <section id="logo"></section>
             {props.children}
         </header>
     );
