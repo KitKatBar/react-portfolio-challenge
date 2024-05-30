f4296057b8b74fbc9555a84d86c959c9ce550b1a
Katriel Chiu <katriel_chiu@msn.com>
2024-05-29 20:31:10 -0700

    fixed formatting

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,30 +4,28 @@ import './style.css'
 function Footer() {
     return (
         <footer>
-            
-                    <address class="contact">
-                        <ul>
-                            <li>
-                                <a id="phone" href="tel:my-number-here"></a>
-                            </li>
-                            <li>
-                                <a id="email" href="mailto:katriel_chiu@msn.com"></a>
-                            </li>
-                            <li>
-                                <a id="github" href="https://github.com/KitKatBar/"></a>
-                            </li>
-                            <li>
-                                <a id="linkedin" href="https://www.linkedin.com/in/katriel-chiu-a8592aa8/"></a>
-                            </li>
-                            <li>
-                                <a id="discord" href="https://discord.com/users/117181812450525190"></a>
-                            </li>
-                            <li>
-                                <a id="youtube" href="https://www.youtube.com/channel/UCgPOWio1gCV7e5ohrsP-Qgw"></a>
-                            </li>
-                        </ul>
-                    </address>
-               
+            <address class="contact">
+                <ul>
+                    <li>
+                        <a id="phone" href="tel:my-number-here"></a>
+                    </li>
+                    <li>
+                        <a id="email" href="mailto:katriel_chiu@msn.com"></a>
+                    </li>
+                    <li>
+                        <a id="github" href="https://github.com/KitKatBar/"></a>
+                    </li>
+                    <li>
+                        <a id="linkedin" href="https://www.linkedin.com/in/katriel-chiu-a8592aa8/"></a>
+                    </li>
+                    <li>
+                        <a id="discord" href="https://discord.com/users/117181812450525190"></a>
+                    </li>
+                    <li>
+                        <a id="youtube" href="https://www.youtube.com/channel/UCgPOWio1gCV7e5ohrsP-Qgw"></a>
+                    </li>
+                </ul>
+            </address>
         </footer>
     );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './style.css'
 
 function Header() {
-
+    return (
+        <header>
+            
+        </header>
+    );
 }
 
 export default Header;
