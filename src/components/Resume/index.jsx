64a335c0a5ee488ee77d65f17a276c3c6711cf4e
Katriel Chiu <katriel_chiu@msn.com>
2024-05-31 00:33:42 -0700

    added a resume that can be downloaded by clicking an icon

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -3,7 +3,21 @@ import './style.css'
 
 function Resume() {
     return (
-        <div>resume page</div>
+        <section class="resume-content">
+            <h2>
+                Resume
+            </h2>
+            <section className="download-resume">
+                <a id="download-icon" href="../../assets/resume/katriel-chiu-resume.pdf" download> </a>
+            </section>
+            <h3>Click me to download!</h3>
+            <h2>
+                Skills & Profeciencies
+            </h2>
+            <div>
+                
+            </div>
+        </section>
     );
 }
 
