a60ae6bb24861dcd2c33d6cc61c0599e6f54b0f0
Katriel Chiu <katriel_chiu@msn.com>
2024-06-10 04:25:11 -0700

    updated projects as a singular component

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css'
+import Projects from './Projects';
 
 function Portfolio() {
     return (
@@ -14,49 +15,7 @@ function Portfolio() {
             <div>
                 {/* Flex display section for the projects */}
                 <section class="section-flex">
-                    {/* Flex display container to hold different elements for each project */}
-                    <a id="data-rebels" class="project-flex" href="https://datarebels.onrender.com/">
-                        <article>
-                            <h3>Data Rebels</h3>
-                            <span>Handlebars/CSS/JavaScript</span>
-                            <a id="github-link" href="https://github.com/Odesii/dataRebels"> </a>
-                        </article>
-                    </a>
-                    <a id="tech-blog" class="project-flex" href="https://github.com/KitKatBar/tech-blog-challenge">
-                        <article>
-                            <h3>Tech Blog</h3>
-                            <span>Handlebars/CSS/JavaScript</span>
-                            <a id="github-link" href="https://tech-blog-challenge.onrender.com"> </a>
-                        </article>
-                    </a>
-                    <a id="soccer-info" class="project-flex" href="https://moe1362.github.io/soccer-info/">
-                        <article>
-                            <h3>Soccer Info</h3>
-                            <span>HTML/CSS/JavaScript</span>
-                            <a id="github-link" href="https://github.com/Moe1362/soccer-info"> </a>
-                        </article>
-                    </a>
-                    <a id="weather-dashboard" class="project-flex" href="https://kitkatbar.github.io/weather-dashboard-challenge/">
-                        <article>
-                            <h3>Weather Dashboard</h3>
-                            <span>HTML/CSS/JavaScript</span>
-                            <a id="github-link" href="https://github.com/KitKatBar/weather-dashboard-challenge"> </a>
-                        </article>
-                    </a>
-                    <a id="text-editor" class="project-flex" href="https://text-editor-challenge-c0we.onrender.com/">
-                        <article>
-                            <h3>Text Editor</h3>
-                            <span>HTML/CSS/Javascript</span>
-                            <a id="github-link" href="https://github.com/KitKatBar/text-editor-challenge"> </a>
-                        </article>
-                    </a>
-                    <a id="blog" class="project-flex" href="https://kitkatbar.github.io/personal-blog-challenge/">
-                        <article>
-                            <h3>Personal Blog</h3>
-                            <span>HTML/CSS/JavaScript</span>
-                            <a id="github-link" href="https://github.com/KitKatBar/personal-blog-challenge"> </a>
-                        </article>
-                    </a>
+                    <Projects />
                 </section>
             </div>
         </section>
